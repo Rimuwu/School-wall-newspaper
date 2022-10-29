2727be98caab4212a4ab493fdb3747be5bfcf197
AS1 <arseniykofanov@gmail.com>
2022-10-29 17:24:12 +0300

    Использование общего метода для главной страницы и релиза

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'main' ),
-    path('releases/', views.releases, name = 'releases' ),
-    
+    path('', views.show_release, name = 'main' ),
+    path('releases/', views.show_releases, name = 'releases' ),
     path('post/<int:post_id>/', views.show_post, name = 'post' ),
     path('release/<int:release_id>/', views.show_release, name = 'release' )
 ]
