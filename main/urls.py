bea1df75bb8c436342367b6084d57e9ea623bec2
AS1 <arseniykofanov@gmail.com>
2022-11-12 17:09:12 +0300

    slug

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.show_release, name = 'main' ),
     path('releases/', views.show_releases, name = 'releases' ),
-    path('post/<int:post_id>/', views.show_post, name = 'post' ),
-    path('release/<int:release_id>/', views.show_release, name = 'release' )
+    path('post/<str:slug>/', views.show_post, name = 'post' ),
+    path('release/<str:slug>/', views.show_release, name = 'release' )
 ]
