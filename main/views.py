334fd5d1a20532909385a5d46f21a6e0ef9ac628
AS1 <arseniykofanov@gmail.com>
2022-11-12 17:09:48 +0300

    slug, get_object_or_404,

--- main/views.py
+++ main/views.py
@@ -1,44 +1,26 @@
-from turtle import pos
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post, Release, LinkPosts, LinkUsers
+from .models import LinkPosts, Post, Release
 
 
-def show_release(request, release_id = None):
-    release = None
-    posts = []
-    users = []
+def show_release(request, slug = None):
 
-    if release_id != None:
-        releases = Release.objects.filter(id = release_id)
-
+    if slug is not None:
+        releases = Release.objects.filter(slug = slug)
+        release = get_object_or_404(Release, slug = slug)
     else:
         releases = Release.objects.filter(visibility = 1).order_by('position')
-
-    if len(releases) != 0:
         release = releases[0]
-        posts_links = LinkPosts.objects.filter(release_link = release).order_by('position')
-        users = LinkUsers.objects.filter(release_link = release).order_by('position')
 
-        for post in posts_links:
-            posts.append(post.post_link)
+    return render( request, 'main/release.html', {'release': release})
 
-    return render( request, 'main/release.html',
-            { 'release': release,
-              'posts': posts,
-              'members': users
-            }
-    )
-
-
-def show_post(request, post_id):
-    post = Post.objects.filter(id = post_id)[0]
+def show_post(request, slug):
+    post = get_object_or_404(Post, slug = slug)
     link_set = LinkPosts.objects.filter(post_link = post)
+    release = None
     
     if len(link_set) > 0:
         release = link_set[0].release_link
-    else:
-        release = None
 
     return render( request, 'main/post.html',
             { 'release': release,
@@ -48,9 +30,5 @@ def show_post(request, post_id):
 
 def show_releases(request):
     releases = Release.objects.filter(visibility = 1).order_by('position')
-
-    return render( request, 'main/releases.html',
-            { "releases": releases,
-            }
-    )
+    return render( request, 'main/releases.html', {"releases": releases})
 
