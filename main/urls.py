7a72705cb35ef36456d85a17d8b7b330627a0900
AS1 <arseniykofanov@gmail.com>
2022-11-20 17:46:02 +0300

    -

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_release, name = 'main' ),
-    path('releases/', views.show_releases, name = 'releases' ),
-    path('post/<str:slug>/', views.show_post, name = 'post' ),
-    path('release/<str:slug>/', views.show_release, name = 'release' )
+    path('', views.show_release, name='main'),
+    path('releases/', views.show_releases, name='releases'),
+    path('post/<str:slug>/', views.show_post, name='post'),
+    path('release/<str:slug>/', views.show_release, name='release')
 ]
