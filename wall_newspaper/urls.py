239d79cf846a7983ae83f9b8f15e7e1543249b3c
AS1 <arseniykofanov@gmail.com>
2022-12-12 19:01:07 +0300

    Ссылка на обработчики ошибок

--- wall_newspaper/urls.py
+++ wall_newspaper/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import handler403, handler404, handler500
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 from . import settings
 
 urlpatterns = [
@@ -10,3 +12,9 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler403 = 'main.views.error_403'
+handler404 = 'main.views.error_404'
+handler500 = 'main.views.error_500'
