756e10d50f36aa1a7d27b6bf0f2ed526f0892f8b
AS1 <arseniykofanov@gmail.com>
2022-12-12 18:58:32 +0300

    Изменён путь для поста

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.show_release, name='main'),
     path('releases/', views.show_releases, name='releases'),
-    path('post/<str:slug>/', views.show_post, name='post'),
+    path('<str:slug>/', views.show_post, name='post'),
     path('release/<str:slug>/', views.show_release, name='release')
 ]
